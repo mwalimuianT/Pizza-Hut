abeea405cac659b213312417a3f738bc0621838f
mwalimuianT <mwalimuiant@gmail.com>
2019-11-11 12:11:42 +0300

    added a README.md document

---
+++ README.md
@@ -0,0 +1,32 @@
+# Project name
+## Pizza Hut
+
+## Description
+- A website that sells Pizza online and let's customers spice their favourite pizza with different toppings. Customers order, set their destination and their package is delivered within minutes.
+
+## Author
+- Ian Mwalimu a Full-stack web developer
+## Setup instructions
+- You can fork the application from github
+- Open your terminal in your PC and type in the search button and search for terminal
+- Double click terminal to open it. Type pwd to see the point you are in your Pc.
+- Feed in the command sudo apt-get update. Then key in sudo apt Pizza Hut
+## BDD
+- If the user keys nothing on the form it should display invalid format advising them to input the respective correct details.
+- The customer gets a confirmation message that the location He/She picked will the sestination at an estimated span of time.
+- The customer recieves a confirmation the the order has been received
+
+## Technologies used
+- HTML
+- CSS 
+- Bootstrap
+- javascript
+-jQuery
+
+## Contact 
+- bookertian9@gmail.com
+- mwalimuiant@gmail.com
+- mwalimuianT.github
+
+## License
+[MIT] Copyright(Ian Mwalimu)
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,43 @@
-function Pizza (crust, size) {
-    this.crust = crust;
-    this.size = size;
-    this.toppings = [];
-    this.size = size;
-    this.price = 0
-}
+// function Pizza (crust, size) {
+//     this.crust = crust;
+//     this.size = size;
+//     this.toppings = [];
+//     this.size = size;
+//     this.price = 0
+// }
 
-Pizza.prototype.cost = function() {
-    var price = 0;
-    if (this.size === "Small") {
-        price += 5;
-    }else if (this.size === "Medium") {
-        price += 7;
-    }else if (this.size === "Large") {
-        price += 12;
-    } else {
-        price += 15;
-    }
-    for (var i = 0; i < this.toppings.length; i++) {
-        price += 4;
-    }
-    if (this.crust === "Crispy") {
-        price += 2;
-    }else if (this.crust === "Stuffed") {
-        price += 4;
-    }else if (this.crust === "Criapy") {
-        price += 3;
-    }else if (this.crust === "Glutten-free") {
-        price += 5;
-    }else {
-        price =1;
-    }
-}
+// Pizza.prototype.cost = function() {
+//     var price = 0;
+//     if (this.size === "Small") {
+//         price += 5;
+//     }else if (this.size === "Medium") {
+//         price += 7;
+//     }else if (this.size === "Large") {
+//         price += 12;
+//     } else {
+//         price += 15;
+//     }
+//     for (var i = 0; i < this.toppings.length; i++) {
+//         price += 4;
+//     }
+//     if (this.crust === "Crispy") {
+//         price += 2;
+//     }else if (this.crust === "Stuffed") {
+//         price += 4;
+//     }else if (this.crust === "Criapy") {
+//         price += 3;
+//     }else if (this.crust === "Glutten-free") {
+//         price += 5;
+//     }else {
+//         price =1;
+//     }
+// }
 
 
 
+// var toppings = function() {
+//     var toppings = document.getElementsByClassName("toppings")
+//     if (toppings== checked) {
+//         alert("Kindly choose")
+//     }
+// }
