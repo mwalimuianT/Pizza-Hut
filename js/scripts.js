82a2de31f107631ceb7d55e7877bbe69c5a999f9
mwalimuianT <mwalimuiant@gmail.com>
2019-11-11 09:15:10 +0300

    added javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function pizza (crust, size) {
+function Pizza (crust, size) {
     this.crust = crust;
     this.size = size;
     this.toppings = [];
@@ -6,3 +6,15 @@ function pizza (crust, size) {
     this.price = [0]
 }
 
+Pizza.prototype.cost = function() {
+    var price = 0;
+    if (this.size === "Small") {
+        price += 5;
+    }else if (this.size === "Medium") {
+        price += 7;
+    }else if (this.size === "Large") {
+        price += 12;
+    } else {
+        price += 15;
+    }
+}
